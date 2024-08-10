cedd556a988848fd8ab5f03824b074005551a47a
GyungA <gga612@naver.com>
2024-08-10 20:29:16 +0900

    Feat: Sign Up

--- mommymomeats/urls.py
+++ mommymomeats/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from mommymomeatsapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', views.signup_view, name='signup'),
 ]
--- mommymomeatsapp/models.py
+++ mommymomeatsapp/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pregnancy_start_date = models.DateField(null=True, blank=True)
+    child_birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username;
