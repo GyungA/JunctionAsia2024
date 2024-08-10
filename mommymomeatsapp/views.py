df884cb42917f84e0aa613d72a43feec85d8931c
GyungA <gga612@naver.com>
2024-08-10 22:37:55 +0900

    Feat: Logout

--- mommymomeatsapp/views.py
+++ mommymomeatsapp/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
@@ -27,3 +27,7 @@ def login_view(request):
         else:
             return render(request, 'login.html', {'error': 'Invalid Credentials'})
     return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
